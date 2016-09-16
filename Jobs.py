f3f86f03f3bab4dce02a2cdd8bba0404ee05c8a1
jordanchan168 <jchan16@stuy.edu>
2016-09-16 01:47:47 -0400

    Update Jobs.py

--- Jobs.py
+++ Jobs.py
@@ -11,14 +11,23 @@ def randjob():
     s = s[1:len(s)-2]
 
     #make a dictionary
-    dict = {}
+    dict = {"a":50, "b":10}
     for job in s:
         #the Occupation is the string job up to the last comma, the percent is whats after the last comma
         if (job[0] == '"'):
             dict[job[1:job.rfind('"')]] = float(job[job.rfind(',')+1:])
         else:
             dict[job[:job.rfind(',')]] = float(job[job.rfind(',')+1:])
-    return dict
+    return weightedrand(dict)
 
+def weightedrand(dict):
+    r = random.random() * 99.8
+    start = 0
+    for job in dict:
+        stop = start + dict[job]
+        if (start <= r < stop):
+            return job
+        else:
+            start = stop
 
-print (randjob())
+print randjob()
